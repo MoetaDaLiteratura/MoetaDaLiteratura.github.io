e91d16795d303e9c41e15d3dbd99d694ac29346b
ElrikSouza <38365856+ElrikSouza@users.noreply.github.com>
2018-08-30 20:38:08 -0400

    email test

--- dados.php
+++ dados.php
@@ -6,23 +6,22 @@
     </head>
     <body>
         <?php
-        $nome=$_POST['name'];
-        $email=$_POST['email'];
-        $message=$_POST['message'];
-        require 'vendor/autoload.php';
+        ini_set('display_errors', 1);
 
-        $from = new SendGrid\Email(null, "likemeleo7@gmail.com");
-        $subject = "Mensagem de contato";
-        $to = new SendGrid\Email(null, "sonleo2013@gmail.com");
-        $content = new SendGrid\Content("text/html", "Olá ADM, <br><br>Nova mensagem de contato<br><br>Nome: $nome <br> Email: $email <br>Mensagem: <br> $message");
-        $mail = new SendGrid\Mail($from, $subject, $to, $content);
-        
-        //Necessário inserir a chave
-        $apiKey = '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';
-        $sg = new \SendGrid($apiKey);
+        error_reporting(E_ALL);
+        $from = "likemeleo7@gmail.com";
 
-        $response = $sg->client->mail()->send()->post($mail);
-        #echo "index.html.element-overlay panels-scope stretched-to-fill"
+        $to = "sonleo2013@gmail.com";
+
+        $subject = "Verificando o correio do PHP";
+
+        $message = "O correio do PHP funciona bem";
+
+        $headers = "De:". $from;
+
+        mail($to, $subject, $message, $headers);
+
+        echo "A mensagem de e-mail foi enviada.";
         header('Location: obrigado.html');
         ?>
     </body>
