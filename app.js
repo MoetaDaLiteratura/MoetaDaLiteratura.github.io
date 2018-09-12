5cd75e175013dde6d26cf06ef4a4d4cbf99aef97
ElrikSouza <38365856+ElrikSouza@users.noreply.github.com>
2018-09-12 17:44:22 -0400

    code clean up

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ var app = angular.module('moita', [
     'ngRoute',
     'ngResource',
     'ngCookies',
-    //'toastr',
-    //'angularScreenfull',
-    //'ng-sortable',
-    //'angularUtils.directives.dirPagination',
-    //'ngCropper',
-    //'angularFileUpload',
-    //'ui.bootstrap',
-    //'ngMask',
     'ngMap',
     'ngSanitize'
 ]);
@@ -20,8 +12,6 @@ app.config([
     function ($routeProvider, $httpProvider) {
         $httpProvider,
         $routeProvider
-            //.when('/', { templateUrl: 'pages/about/views/list.html', title: ['Moita'] }) 
-            .when('/poseia', { templateUrl: 'pages/about/views/poesia.html', title: [] })
             .when('/', { templateUrl: 'controllers/menuDeObras.html', title: [] })
     }
 ]);
