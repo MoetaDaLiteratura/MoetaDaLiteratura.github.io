dffa3992c01d5767603f4cd1239303d28736c114
ElrikSouza <38365856+ElrikSouza@users.noreply.github.com>
2018-10-06 20:12:51 -0400

    newObras.html update, minor fixes for newIndex.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,12 +9,34 @@ function closeSidebar() {
 
 function renderObras() {
     const obras = Obras();
-    const html = obras.obras.map(it => `
+    const html = obras.obras.map((it, index) => `
         <div class="card">
-            ${it.autor}
+            <span class="card-title">${it.titulo}</span> -
+            <span class="card-subtitle">${it.autor}</span>
+            <div class="card-content">
+                <p>${it.conteudo.split(" ").slice(1,25).join(" ")} ...</p>
+            </div>
+            <div class="card-actions">
+                <button class="btn btn-red btn-md" onclick="openDialogText(${index})">Ver mais</button>
+            </div>
         </div>
     `).reduce((a,b) => a + b);
     document.getElementById('obras').innerHTML=html;
+}
+
+function openDialogText(id) {
+    const dialog = document.getElementsByClassName('dialog-text')[0];
+    const obra = Obras().obras[id];
+    const title = document.getElementById("dialog-text-title");
+    const content = document.getElementById("dialog-text-content");
+    title.innerHTML = obra.titulo;
+    content.innerHTML = obra.conteudo;
+    dialog.className = 'dialog-text dialog-text-open';
+}
+
+function closeDialogText() {
+    const dialog = document.getElementsByClassName('dialog-text-open')[0];
+    dialog.className = 'dialog-text';
 }
 
 window.addEventListener('load', event => renderObras());
