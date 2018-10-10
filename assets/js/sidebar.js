7cae1e793f1b731f42dc14befe1bbe1261f227eb
ElrikSouza <38365856+ElrikSouza@users.noreply.github.com>
2018-10-09 20:10:05 -0400

    new homepage test

---
+++ assets/js/sidebar.js
@@ -0,0 +1,9 @@
+function sidebarOpen() {
+    const sidebar = document.getElementsByClassName('sidebar')[0];
+    sidebar.className = 'sidebar sidebar-open';
+}
+
+function sidebarClose() {
+    const sidebar = document.getElementsByClassName('sidebar-open')[0];
+    sidebar.className = 'sidebar';
+}
